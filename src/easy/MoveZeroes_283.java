3d77ccf911dba7635cad05ecb50a8e09e92901a6
robin <403704156@qq.com>
2016-11-14 17:17:32 +0800

    2016/11/14_01

---
+++ src/easy/MoveZeroes_283.java
@@ -0,0 +1,25 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * Created by ping.wu on 2016/11/14.
+ */
+public class MoveZeroes_283 {
+    public static void main(String[] args) {
+        
+    }
+
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+        for (; pos < nums.length; pos++) {
+            nums[pos] = 0;
+        }
+    }
+}
