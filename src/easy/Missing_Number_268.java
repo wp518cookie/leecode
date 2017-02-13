a9865de8396e4bff4dec353a56fe2e1ece7e8847
robin <403704156@qq.com>
2017-02-13 17:53:51 +0800

    2017/02/13

---
+++ src/easy/Missing_Number_268.java
@@ -0,0 +1,19 @@
+package easy;
+
+/**
+ * Created by ping.wu on 2017/2/13.
+ */
+public class Missing_Number_268 {
+    public static void main(String[] args) {
+        int[] nums = {0};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result += i - nums[i];
+        }
+        return result + nums.length;
+    }
+}
