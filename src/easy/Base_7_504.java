06bb10ae596a095eb7e53086229981a1c5b2b9c0
robin <403704156@qq.com>
2017-02-22 17:56:32 +0800

    2017/2/22

---
+++ src/easy/Base_7_504.java
@@ -0,0 +1,23 @@
+package easy;
+
+/**
+ * Created by ping.wu on 2017/2/13.
+ */
+public class Base_7_504 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String convertToBase7(int num) {
+//        String result = "";
+//        if (num < 0) {
+//            num = num * (-1);
+//        }
+//        while (num >= 0) {
+//            if (num % 7 != 0) {
+//                result
+//            }
+//        }
+        return null;
+    }
+}
---
+++ src/easy/Largest_Palindrome_Product_479.java
@@ -0,0 +1,42 @@
+package easy;
+
+/**
+ * Created by ping.wu on 2017/2/16.
+ */
+public class Largest_Palindrome_Product_479 {
+    public static void main(String[] args) {
+        System.out.println(largestPalindrome(5));
+    }
+
+    public static int largestPalindrome(int n) {
+        long a = 1;
+        while (n-- > 0) {
+            a = a * 10;
+        }
+        long sum = a * a - 1;
+        long max = a - 1;
+        long min = a * a / 100;
+        while (sum > min) {
+            if (judgePalindrome(sum)) {
+                for (long i = max; i >= a / 10; i --) {
+                    if (sum % i == 0 && (sum / i) < max) {
+                        return (int)sum % 1337;
+                    }
+                }
+            }
+            sum--;
+        }
+        return 0;
+    }
+
+    public static boolean judgePalindrome(long n) {
+        char[] temp = String.valueOf(n).toCharArray();
+        int length = temp.length;
+        for (int i = 0; i < length / 2; i++) {
+            if (temp[i] != temp[length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
