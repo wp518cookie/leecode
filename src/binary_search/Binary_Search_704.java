3f0f852f0a3f6831723c1592202162eab8b08be1
wp <403704156@qq.com>
2019-07-25 14:17:08 +0800

    fighting

---
+++ src/binary_search/Binary_Search_704.java
@@ -0,0 +1,34 @@
+package binary_search;
+
+/**
+ * @author wuping
+ * @date 2019-07-25
+ * https://leetcode.com/problems/binary-search/
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ */
+
+public class Binary_Search_704 {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums[0] > target || nums[nums.length - 1] < target) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start < end) {
+            mid = start + ((end - start) >> 1);
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return nums[start] == target ? start : -1;
+    }
+}
